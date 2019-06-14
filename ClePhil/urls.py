b7b4263c7d590c7bcb15090570a5c559394e5d81
Holly Witchey <hollywitchey@Hollys-MacBook-Air.local>
2019-06-14 17:59:02 -0400

    SecondBranch

--- ClePhil/urls.py
+++ ClePhil/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+#from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from django.views.generic import *
+
+class Index(TemplateView):
+    template_name= 'index.html'
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path ('', Index.as_view(), name='index'),
 ]
